394fea26bfe881d24e0bc9eb0c7966f34cbc871d
yhjiang <yonghuajiang@hotmail.com>
2018-06-19 22:19:18 -0400

    update my_custome_player

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,6 +1,7 @@
 
 from sample_players import DataPlayer
-
+import random
+from isolation.isolation import _WIDTH, _HEIGHT
 
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -23,6 +24,7 @@ class CustomPlayer(DataPlayer):
     **********************************************************************
     """
     def get_action(self, state):
+
         """ Employ an adversarial search technique to choose an action
         available in the current state calls self.queue.put(ACTION) at least
 
@@ -39,11 +41,72 @@ class CustomPlayer(DataPlayer):
           Refer to (and use!) the Isolation.play() function to run games.
         **********************************************************************
         """
-        # TODO: Replace the example implementation below with your own search
-        #       method by combining techniques from lecture
-        #
-        # EXAMPLE: choose a random move without any search--this function MUST
-        #          call self.queue.put(ACTION) at least once before time expires
-        #          (the timer is automatically managed for you)
-        import random
-        self.queue.put(random.choice(state.actions()))
+        if state.ply_count < 2: self.queue.put(random.choice(state.actions()))
+        for depth in range(3,100):
+            best_action = self.alpha_beta_search(state, depth) 
+            self.queue.put(best_action)
+
+    def alpha_beta_search(self, state, depth):
+
+        def min_value(state, alpha, beta, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: 
+                value = self.score(state)
+                return value
+            value = float("inf")
+            action_score = [(action,self.score(state.result(action))) for action in state.actions()]
+            action_score.sort(key=lambda tup: tup[1],reverse=True)
+            action_list = [action[0] for action in action_score]
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), alpha, beta, depth - 1))
+                if value <= alpha:
+                    return value
+                beta = min(beta, value)
+            return value
+
+        def max_value(state, alpha, beta, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("-inf")
+            action_score = [(action,self.score(state.result(action))) for action in state.actions()]
+            action_score.sort(key=lambda tup: tup[1])
+            action_list = [action[0] for action in action_score]
+            for action in action_list:
+                value = max(value, min_value(state.result(action), alpha, beta, depth - 1))
+                if value >= beta:
+                    return value
+                alpha = max(alpha,value)
+            return value
+        alpha = float("-inf")
+        beta = float("inf")
+        best_score = float("-inf")
+        best_move = None
+        for action in state.actions():
+            v = min_value(state.result(action), alpha, beta, depth - 1)
+            alpha = max(alpha,v)
+            if v > best_score:
+                best_score = v
+                best_move = action
+        return best_move
+
+  
+    def score(self, state):
+        sim_score = 0
+        for _ in range(1000):
+            sim_score += self.mc_simulation(state)
+        return sim_score/1000    
+        
+    def mc_simulation(self,state):
+        if state.terminal_test(): 
+            if state.utility(self.player_id) == float('inf'): 
+                return 1
+            else: return 0
+        else: 
+            legal_action = state.actions()
+            action = random.choice(legal_action)
+            new_state = state.result(action)
+            return self.mc_simulation(new_state)
+        
+    
+           
+        
--- Projects/3_Adversarial Search/run_match.py
+++ Projects/3_Adversarial Search/run_match.py
@@ -17,7 +17,7 @@ from my_custom_player import CustomPlayer
 logger = logging.getLogger(__name__)
 
 NUM_PROCS = 1
-NUM_ROUNDS = 5  # number times to replicate the match; increase for higher confidence estimate
+NUM_ROUNDS = 50  # number times to replicate the match; increase for higher confidence estimate
 TIME_LIMIT = 150  # number of milliseconds before timeout
 
 TEST_AGENTS = {
